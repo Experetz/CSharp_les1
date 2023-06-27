4d0605b97021d14fb97bb2ac6b1902eda15f8c15
Expertz <xeniapliner@gmail.com>
2023-06-27 15:25:21 +0300

    005 Condition

---
+++ Examples_005_Condition/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What is your name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+ Console.WriteLine("Wow, it's Masha!!!");
+}
+else 
+{
+ Console.Write($"Hello {username}");
+ }
